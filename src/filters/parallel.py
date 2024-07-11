48b524881b4e29c165eee3ac2f73ed351b5d86f7
KH <khenkelm@sfu.ca>
2024-07-11 14:09:23 -0700

    added round of comments to parallel.py

--- src/filters/parallel.py
+++ src/filters/parallel.py
@@ -2,11 +2,20 @@ from .filter import Filter
 import numpy as np
 
 class ParallelFilter(Filter):
+    # frequencies is a list of -centre- frequencies, e.g. [60, 125, 250, 500, 1000, 2000, 4000, 8000, 16000].
+    # gains is a list of gains for each of the frequencies, e.g. [6, 0, 0, 0, 0, 0, -5, 0, 0].
     def __init__(self, frequencies, gains):
         super().__init__(frequencies, gains)
 
+    # samples is a list of direct samples from the time-domain file.
+    # frame_rate is by default 44100 samples of audio per second.
     def apply(self, samples, frame_rate):
+
+        # fft_samples is a huge list of complex numbers (as many as there were audio samples), representing
+        # the magnitude and phase of every single frequency component. (strictly speaking, not every single
+        # frequency component as there are an infinite number, but it has as many as there were time samples).
         fft_samples = np.fft.fft(samples)
+        # freqs are the equivalent frequencies for each frequency component (i.e., the evenly-spaced domain of fft_samples).
         freqs = np.fft.fftfreq(len(fft_samples), 1.0/frame_rate)
         
         for frequency, gain in zip(self.frequencies, self.gains):
