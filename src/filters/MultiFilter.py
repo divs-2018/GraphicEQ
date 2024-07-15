522b0a69b8ef939dd91781cbb4117d6e81ed9132
Ian <thecruik@gmail.com>
2024-07-15 01:10:53 -0700

    Create MultiFilter Class

---
+++ src/filters/MultiFilter.py
@@ -0,0 +1,6 @@
+from src.filters.Filter import Filter
+
+class MultiFilter(Filter):
+    def __init__(self, control_frequencies, gains):
+        self.control_frequencies = control_frequencies
+        self.gains = gains
--- src/filters/filter.py
+++ src/filters/filter.py
@@ -1,7 +1,3 @@
 class Filter:
-    def __init__(self, control_frequencies, gains):
-        self.control_frequencies = control_frequencies
-        self.gains = gains
-
     def apply(self, samples, frame_rate):
         raise NotImplementedError("This method should be overridden by subclasses")
