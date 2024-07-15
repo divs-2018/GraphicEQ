b09236d23fadf28488edd748b1aca96c7f383613
Ian <thecruik@gmail.com>
2024-07-15 01:18:51 -0700

    Remove unnecessary for loop in ShelfFilter.apply()

--- src/filters/ShelfFilter.py
+++ src/filters/ShelfFilter.py
@@ -12,18 +12,14 @@ class ShelfFilter(Filter):
         self.horiz_scale = horiz_scale
 
     def H_shelf(self, f, gain, centre_freq_Hz, B):
-        if(f < 0):
-            f *= -1
-        
-        return 1 + (gain - 1) / (1 + np.pow(f / centre_freq_Hz, B))
+        return 1 + (gain - 1) / (1 + np.pow(np.abs(f) / centre_freq_Hz, B))
 
     def apply(self, samples, frame_rate):
         fft_samples = np.fft.fft(samples)
         freqs = np.fft.fftfreq(len(fft_samples), 1.0 / frame_rate)
         
         filtered_fft_samples = fft_samples
-        for i in range(0, len(freqs)):
-            filtered_fft_samples[i] *= self.H_shelf(freqs[i], self.gain, self.cross_over_freq_Hz, self.horiz_scale)
+        filtered_fft_samples *= self.H_shelf(freqs, self.gain, self.cross_over_freq_Hz, self.horiz_scale)
 
         filtered_samples = np.fft.ifft(filtered_fft_samples)
 
