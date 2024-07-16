fc21166b822d34b650589870051ee3f3a829e7ef
Ian <thecruik@gmail.com>
2024-07-15 23:45:17 -0700

    Reduce number of args for H functions in PeakNotch and Shelf filters

--- src/filters/PeakNotchFilter.py
+++ src/filters/PeakNotchFilter.py
@@ -11,15 +11,15 @@ class PeakNotchFilter(Filter):
         self.centre_freq_Hz = centre_freq_Hz
         self.horiz_scale = horiz_scale
 
-    def H_peakNotch(self, f, gain, centre_freq_Hz, horiz_scale):
-        freq_ratio = np.abs(f / centre_freq_Hz)
-        return 1 + (gain - 1) / np.pow(freq_ratio, horiz_scale * np.log10(freq_ratio))
+    def H_peakNotch(self, f):
+        freq_ratio = np.abs(f / self.centre_freq_Hz)
+        return 1 + (self.gain - 1) / np.pow(freq_ratio, self.horiz_scale * np.log10(freq_ratio))
     
     def apply(self, samples, frame_rate):
         fft_samples = np.fft.fft(samples)
         freqs = np.fft.fftfreq(len(fft_samples), 1.0 / frame_rate)
         
-        filtered_fft_samples = fft_samples * self.H_peakNotch(freqs, self.gain, self.centre_freq_Hz, self.horiz_scale)
+        filtered_fft_samples = fft_samples * self.H_peakNotch(freqs)
         
         filtered_samples = np.fft.ifft(filtered_fft_samples)
 
--- src/filters/ShelfFilter.py
+++ src/filters/ShelfFilter.py
@@ -11,14 +11,14 @@ class ShelfFilter(Filter):
         self.cross_over_freq_Hz = cross_over_freq_Hz
         self.horiz_scale = horiz_scale
 
-    def H_shelf(self, f, gain, cross_over_freq_Hz, B):
-        return 1 + (gain - 1) / (1 + np.pow(np.abs(f) / cross_over_freq_Hz, B))
+    def H_shelf(self, f):
+        return 1 + (self.gain - 1) / (1 + np.pow(np.abs(f) / self.cross_over_freq_Hz, self.horiz_scale))
 
     def apply(self, samples, frame_rate):
         fft_samples = np.fft.fft(samples)
         freqs = np.fft.fftfreq(len(fft_samples), 1.0 / frame_rate)
         
-        filtered_fft_samples = fft_samples * self.H_shelf(freqs, self.gain, self.cross_over_freq_Hz, self.horiz_scale)
+        filtered_fft_samples = fft_samples * self.H_shelf(freqs)
 
         filtered_samples = np.fft.ifft(filtered_fft_samples)
 
